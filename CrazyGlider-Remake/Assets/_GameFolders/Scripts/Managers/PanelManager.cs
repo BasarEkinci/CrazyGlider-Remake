7c4b7a661aaa348099b7b0d8ce66293e85cf5fcc
basarekinci <basarekinci02@outlook.com>
2025-09-26 15:12:13 +0300

    Stats Manager Added

---
+++ CrazyGlider-Remake/Assets/_GameFolders/Scripts/Managers/PanelManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using _GameFolders.Scripts.Enums;
+using DG.Tweening;
+using UnityEngine;
+using static _GameFolders.Scripts.Managers.GameEventManager;
+
+namespace _GameFolders.Scripts.Managers
+{
+    public class PanelManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject statsPanel;
+
+        private void Start()
+        {
+            statsPanel.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            OnPlayerStateChanged += HandlePlayerStateChanged;
+        }
+        
+        private void OnDisable()
+        {
+            OnPlayerStateChanged -= HandlePlayerStateChanged;
+        }
+
+        private void HandlePlayerStateChanged(PlayerState state)
+        {
+            if (state == PlayerState.Flying)
+            {
+                statsPanel.SetActive(true);
+                statsPanel.transform.DOScale(Vector3.zero, 0.1f).From().SetEase(Ease.Linear);
+            }
+        }
+    }
+}
---
+++ CrazyGlider-Remake/Assets/_GameFolders/Scripts/Managers/StatsManager.cs
@@ -0,0 +1,62 @@
+﻿using _GameFolders.Scripts.Enums;
+using TMPro;
+using UnityEngine;
+using static _GameFolders.Scripts.Managers.GameEventManager;
+
+namespace _GameFolders.Scripts.Managers
+{
+    public class StatsManager : MonoBehaviour
+    {
+        [SerializeField] private float seaLevel = -33f;
+        [Header("UI References")] 
+        [SerializeField] private TMP_Text targetDistance;
+        [SerializeField] private TMP_Text distanceText;
+        [SerializeField] private TMP_Text currentHeightText;
+        [SerializeField] private TMP_Text maxHeightText;
+        
+        [Header("Player Reference")]
+        [SerializeField] private Transform playerTransform;
+
+        private Vector3 _initialPosition;
+        private float _maxHeight = 0f;
+        private bool _isFlying;
+        
+        private void OnEnable()
+        {
+            OnPlayerStateChanged += HandlePlayerStateChanged;
+        }
+
+        private void Update()
+        {
+            if (playerTransform != null && _isFlying)
+            {
+                float distance = Vector3.Distance(_initialPosition, playerTransform.position);
+                distanceText.SetText($" {distance:F}");
+                float currentHeight = playerTransform.position.y - seaLevel;
+                currentHeight = Mathf.Max(currentHeight, 0f);
+                currentHeightText.SetText($" {currentHeight:F}");
+                if (currentHeight > _maxHeight)
+                {
+                    _maxHeight = currentHeight;
+                }
+                maxHeightText.SetText($" {Mathf.Max(currentHeight, _maxHeight):F}");
+            }
+        }
+        private void OnDisable()
+        {
+            OnPlayerStateChanged -= HandlePlayerStateChanged;
+        }
+        private void HandlePlayerStateChanged(PlayerState state)
+        {
+            if (state == PlayerState.Flying)
+            {
+                _initialPosition = playerTransform.position;
+                _isFlying = true;
+            }
+            else if (state == PlayerState.Sink)
+            {
+                _isFlying = false;
+            }
+        }
+    }
+}
